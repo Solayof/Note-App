aacf84a024f1a69ac43eff9901bffc89cf3f72c2
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-01-07 12:24:17 +0100

    update

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <h1>
+      My Notes
+    </h1>
+  `,
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
