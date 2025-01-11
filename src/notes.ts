ac1eb05a0fea2cec6a60e8270a485a005fb21e7f
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-01-11 10:24:06 +0100

    add to the note

--- src/notes.ts
+++ src/notes.ts
@@ -7,7 +7,7 @@ export const NOTES: Note[] = [
   {
     id: 1,
     title: 'Lorem ipsum',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
     id: 2,
